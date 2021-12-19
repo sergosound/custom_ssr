1054247f685dead5f0ce14d4ddfad2671e7b5e7b
sergey.dirko <sergey.dirko@auroraglobal.com>
2021-12-19 16:43:41 +0200

    init 2

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const http = require('http');
+
+const PORT = 8080;
+const hostname = '127.0.0.1';
+
+const server = http.createServer((req, res) => {
+    console.log('Request', req.url);
+    res.end()
+});
+
+server.listen(PORT, hostname, () => {
+    console.log('Server running on port:', PORT);
+});
